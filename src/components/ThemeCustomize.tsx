ed194aa1c1acd356c9c8fe928f2600d2015eebc7
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-04-10 12:34:53 +0300

    last changes

--- src/components/ThemeCustomize.tsx
+++ src/components/ThemeCustomize.tsx
@@ -1,8 +1,42 @@
-import { Box } from "@mui/material";
-
+import { Info } from "@mui/icons-material";
+import { Box, Button, ButtonGroup, colors, createTheme } from "@mui/material";
+import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+const theme2 = createTheme({
+	palette: {
+		secondary: {
+			main: colors.blueGrey[800],
+		},
+	},
+});
 export const ThemeCustomize = () => {
+	const [color, setColor] = useState("main.secondary");
 	return (
-		<Box
-			sx={{ marginTop: "90px", height: 400, bgcolor: "secondary.dark" }}></Box>
+		<ThemeProvider theme={theme2}>
+			<Box
+				sx={{
+					marginTop: "90px",
+					height: 400,
+					bgcolor: color,
+				}}></Box>
+
+			<ButtonGroup size="small" color="info" sx={{ gap: 2, marginTop: 3 }}>
+				<Button variant="contained" onClick={() => setColor("#52cdfd")}>
+					Change color info
+				</Button>
+				<Button variant="contained" onClick={() => setColor("#21eb75")}>
+					Change color success
+				</Button>
+				<Button variant="contained" onClick={() => setColor("#f7e605")}>
+					Change color warning
+				</Button>
+				<Button variant="contained" onClick={() => setColor("#f5342d")}>
+					Change color error
+				</Button>
+				<Button variant="contained" onClick={() => setColor("#ba29fd")}>
+					Change color secondary
+				</Button>
+			</ButtonGroup>
+		</ThemeProvider>
 	);
 };
